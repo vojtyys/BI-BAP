2d2b7350d5c0ae154be08c4bffe62b3dd24aed7e
Vojtech Pail <vojta.pail@seznam.cz>
2018-03-11 20:19:09 +0100

    Added CRC

--- sender.py
+++ sender.py
@@ -2,6 +2,7 @@
 import time
 import serial
 import RPi.GPIO as GPIO
+import crcmod
 
 
 GPIO.setmode(GPIO.BOARD)
@@ -10,15 +11,22 @@ GPIO.output(12, GPIO.LOW)
 
 ser = serial.Serial('/dev/ttyS0')
 ser.baudrate = 9600
-while True:
-    try:
-        data = bytes(input('Enter addres and command or ^C to exit: '), 'ascii')
-        GPIO.output(12, GPIO.HIGH)
-        ser.write(data)
-        time.sleep(0.1)
-        GPIO.output(12, GPIO.LOW)
-    except(KeyboardInterrupt):
-        ser.close()
-        print('\nTerminating program...\n')
-        GPIO.cleanup()
-        exit(143)
+
+crc16 = crcmod.mkCrcFun(0x11021, 0xffff, False, 0x0000)
+
+tmp = bytes.fromhex('01 05 00 00 00')
+crc = crc16(tmp)
+crc = str(hex(crc)).lstrip('0x')
+if (len(crc) != 4):
+    crc = '0' + crc;
+data = bytes(tmp + bytes.fromhex(crc))
+print(data)
+GPIO.output(12, GPIO.HIGH)
+ser.write(data)
+time.sleep(0.1)
+GPIO.output(12, GPIO.LOW)
+
+ser.close()
+print('\nTerminating program...\n')
+GPIO.cleanup()
+exit(0)
