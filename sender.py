bda827db4ab196f445028dd85f3ccc2cf9b3d7d1
Vojtech Pail <vojta.pail@seznam.cz>
2018-03-11 22:27:54 +0100

    added check ack

--- sender.py
+++ sender.py
@@ -11,20 +11,47 @@ GPIO.output(12, GPIO.LOW)
 
 ser = serial.Serial('/dev/ttyS0')
 ser.baudrate = 9600
+ser.timeout = 1
 
 crc16 = crcmod.mkCrcFun(0x11021, 0xffff, False, 0x0000)
 
-tmp = bytes.fromhex('01 05 00 00 00')
+def checkCrc(data):
+    crc = crc16(data[0:5])
+    crc = str(hex(crc)).lstrip('0x')
+    if (len(crc) != 4):
+        crc = '0' + crc
+    crc = bytes.fromhex(crc)
+    tmp = data[5:]
+#  print(tmp)
+    print(crc)
+    if (tmp == crc):
+        return True
+    return False
+	
+tmp = bytes.fromhex('01 05 12 80 f0')
 crc = crc16(tmp)
 crc = str(hex(crc)).lstrip('0x')
 if (len(crc) != 4):
     crc = '0' + crc;
 data = bytes(tmp + bytes.fromhex(crc))
+#data = bytes(tmp + bytes.fromhex('00ff'))
 print(data)
 GPIO.output(12, GPIO.HIGH)
 ser.write(data)
-time.sleep(0.1)
+ser.flush()
 GPIO.output(12, GPIO.LOW)
+
+data = ser.read(7)
+print('Got: ')
+print(data)
+if (len(data) < 7):
+    print('Incorrect ACK')
+    print(data)
+else:
+    if(checkCrc(data) == False):
+        print('Incorrect CRC')
+    else:
+        print('ACK OK')
 
 ser.close()
 print('\nTerminating program...\n')
