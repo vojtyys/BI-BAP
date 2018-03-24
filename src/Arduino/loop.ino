be347d6a29298c64f65f6d74826ac2ea1b20abd6
Vojtěch Pail <vojta.pail@seznam.cz>
2018-03-24 13:36:12 +0100

    integrace prikazu #9

--- src/Arduino/loop.ino
+++ src/Arduino/loop.ino
@@ -1,26 +1,34 @@
+#include <StepMotor.h>
+#include <Relay.h>
+#include <Led.h>
 #include <FastCRC.h>
 
 
 #define EN 2
-#define ADDR 1
+#define ADDR 10
 #define CMDLEN 7
-#define RX 10
-#define TX 11
+#define LED 13
 
 FastCRC16 CRC16;
+Led light(3);
+Relay socket(4);
+StepMotor window(5, 6, 7, 8);
 
 uint8_t cnt = 0;
 
+
+
+
 void setup() {
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
   Serial.begin(9600);
   //Serial1.begin(9600);
   pinMode(EN, OUTPUT);
   digitalWrite(EN, LOW);
 }
 
-enum State { // <-- the use of typedef is optional
+enum State {
   WAIT,
   CHECK,
   RUN
@@ -33,43 +41,21 @@ State next_state = curr_state;
 void loop() {
   switch (curr_state) {
     case WAIT:
-      //Serial.println("Waiting for cmd");
       if (Serial.available()) {
         if (Serial.readBytes(cmd, CMDLEN) != CMDLEN) {
           next_state = WAIT;
-         // Serial.println("Incorrect len of cmd");
         } else {
-          /*Serial.print("CMD: ");
-          Serial.print(cmd[0], HEX);
-          Serial.print(" ");
-          Serial.print(cmd[1], HEX);
-          Serial.print(" ");
-          Serial.print(cmd[2], HEX);
-          Serial.print(" ");
-          Serial.print(cmd[3], HEX);
-          Serial.print(" ");
-          Serial.print(cmd[4], HEX);
-          Serial.print(" ");
-          Serial.print(cmd[5], HEX);
-          Serial.print(" ");
-          Serial.println(cmd[6], HEX);*/
           next_state = CHECK;
-
-          //delay(500);
         }
       }
-      //delay(500);
       break;
 
     case CHECK:
       if (cmd[0] != ADDR) {
-        //Serial.println("Address invalid");
         next_state = WAIT;
       } else if (!checkCRC(cmd)) {
-        //Serial.println("CRC invalid");
         next_state = WAIT;
       } else if (cmd[1] != cnt) {
-        //Serial.println("Redundant CMD");
         sendAck(cmd[1]);
         next_state = WAIT;
       } else {
@@ -80,10 +66,66 @@ void loop() {
       break;
 
     case RUN:
-      //Serial.println("Running commands");
-      digitalWrite(13, HIGH);
-      delay(500);
-      digitalWrite(13, LOW);
+      switch (cmd[2]) {
+        case 0:  //light
+          switch (cmd[3]) {
+            case 0:  //on
+              light.on();
+              break;
+
+            case 1: //off
+              light.off();
+              break;
+
+            case 2: //dim
+              light.setDim(cmd[4]);
+          }
+          break;
+
+        case 1: //socket
+          switch (cmd[3]) {
+            case 0:  //on
+              socket.on();
+              break;
+
+            case 1: //off
+              socket.off();
+              break;
+          }
+          break;
+
+        case 2:  //boiler
+          /*switch (cmd[3]) {
+              //TODO
+            }*/
+          break;
+
+        case 3: //window
+          /*switch (cmd[3]) {
+              //TODO
+            }*/
+          break;
+
+        case 4: //led
+          switch (cmd[3]) {
+            case 0:
+              digitalWrite(LED, HIGH);
+              break;
+            case 1:
+              digitalWrite(LED, LOW);
+              break;
+          }
+          break;
+          
+        default:
+          for (int i = 0; i < 5; i++) {
+            digitalWrite(LED, HIGH);
+            delay(50);
+            digitalWrite(LED, LOW);
+            delay(50);
+          }
+          break;
+      }
       next_state = WAIT;
       break;
   }
@@ -116,7 +158,7 @@ void sendAck(uint8_t ackCnt) {
   ack[0] = 0x00;  //adresa ridici jednotky
   ack[1] = ackCnt;  //cislo potvrzovaneho ramce
   ack[2] = 0x00;   //ACK
-  ack[3] = 0x00;
+  ack[3] = ADDR;
   ack[4] = 0x00;
   uint16_t crc = getCRC(ack, 5);
   ack[5] = (crc & 0xFF00) >> 8;  //pripojeni CRC
@@ -125,9 +167,9 @@ void sendAck(uint8_t ackCnt) {
   Serial.write(ack, sizeof(ack));
   Serial.flush();
   digitalWrite(EN, LOW);
-  
-  //Serial.println("ACK");
+
 }
+
 
 
 
