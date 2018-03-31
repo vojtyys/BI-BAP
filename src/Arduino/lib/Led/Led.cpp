38de873521b7ba155c6c68e2a3a73533667c4a32
Vojtěch Pail <vojta.pail@seznam.cz>
2018-03-31 19:15:15 +0200

    vylepšení světel #21

--- src/Arduino/lib/Led/Led.cpp
+++ src/Arduino/lib/Led/Led.cpp
@@ -9,24 +9,33 @@ Led.cpp - Implementační soubor knihovny pro ovládání LED.
 Led::Led(int pin) {
   m_pin = pin;                //nastavení čísla pinu
   m_currIntensity = 0;        //intenzita světla je na počátku 0
+  m_lastIntensity = 255;      //pokud bude voláno on před dim, rozsvítí se LED úplně
   pinMode(m_pin, OUTPUT);     //nastavení pinu jako výstup
   digitalWrite(m_pin, LOW);   //led je na začátku vypnutá
+  m_isOn = false;             //led je na začátku vypnutá
 }
 
 //------------------------------------------------------------------------------
 void Led::on() {
-  digitalWrite(m_pin, HIGH);  //zapnutí LED
-  m_currIntensity = 255;      //intenzita je maximální
+	Led::dim(m_lastIntensity);  //zapnuti LED na poslední nastavenou hodnotu
+	m_isOn = true;              //led je zapnutá
 }
 
 //------------------------------------------------------------------------------
 void Led::off() {
-  digitalWrite(m_pin, LOW);  //vypnutí LED
-  m_currIntensity = 0;       //intenzita je nulová
+  Led::dim(0);     //vypnutí led
+  m_isOn = false;  //led je vypnutá
 }
 
 //------------------------------------------------------------------------------
-void Led::setDim(byte intensity) {  //pokud se změní hodnota stmívání, je intenzita osvětlení plynule upravena na novou úroveň
+void Led::setDim(byte intensity) {
+	if (m_isOn) {
+		Led::dim(intensity);  //pokud je LED zapnutá, změňí intenzitu stmívání
+	}
+	m_lastIntensity = intensity;  //po zapnutí se nastaví tato úroveň stmívání
+}
+//------------------------------------------------------------------------------
+void Led::dim(byte intensity) {  //pokud se změní hodnota stmívání, je intenzita osvětlení plynule upravena na novou úroveň
   if (intensity < m_currIntensity) {
     for (int i = m_currIntensity; i >= intensity; --i){
       analogWrite(m_pin, i);
--- src/Arduino/lib/Led/Led.h
+++ src/Arduino/lib/Led/Led.h
@@ -30,15 +30,22 @@ public:
   void off();
   
   /** Metoda pro nastavení intenzity stmívání LED.
+   * Pokud je světlo vypnuté, pouze se nastaví hodnota, která se použije připříštím spuštění, jinak dojde ke změně intenzity osvětlení.
    * Změna intenzity je provedena plynule.
    * @param intensity - Intenzita osvětlení, rozsah 0-255, hodnota 0 odpovídá vypnuté LED, hodnota 255 je maximální intenzita světla.
    */
   void setDim(byte intensity);
 private:
+	//nastaví intnzity na zadanou úroveň
+	void dim(byte intensity);
   //číslo pinu pro ovládání LED
   int m_pin;
-  //aktualní hodnota intenziti osvětlení
-  byte m_currIntensity;    
+  //aktualní hodnota intenzity osvětlení
+  byte m_currIntensity;
+	//předchozí hodnota intenzity osvětlení, nastaví se při volání metody on
+	byte m_lastIntensity;
+	
+	bool m_isOn;  
 };
 
 #endif
