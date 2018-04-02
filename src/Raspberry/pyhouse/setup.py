717f4435214b48ab8579eeee8badfcf06c6ccba2
Vojtěch Pail <vojta.pail@seznam.cz>
2018-04-02 09:40:17 +0200

    pridan python package

---
+++ src/Raspberry/pyhouse/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pyhouse',
+      version='0.1',
+      description='Controll API for smart house',
+      url='http://github.com/vojtyys/BI-BAP',
+      author='Vojtech Pail',
+      author_email='pailvoj1@fit.cvut.cz',
+      packages=['pyhouse', 'pyserial'],
+      zip_safe=False)
