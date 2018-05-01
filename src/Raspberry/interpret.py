bad29f533f97bd68d08c1a4d4d1ba51fa2ad9bb4
Vojtěch Pail <vojta.pail@seznam.cz>
2018-05-01 20:13:50 +0200

    okomentovano,upraven interpret, upraveny vyjimky

--- src/Raspberry/interpret.py
+++ src/Raspberry/interpret.py
@@ -3,12 +3,14 @@
 import os
 import sys
 
+#připojení modulu pyhouse do systémové cesty, aby mohl být importován
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import pyhouse
 
 #slovník zařízení pro jejich přidávání, odebírání a ovládání
 devices = {}
+#funkce pro zobrazení použití
 def showUsage():
     print('''             Usage: add name address - use to add new device
                     del name - use to remove device
@@ -17,69 +19,104 @@ def showUsage():
                     send name remote_device function [parameter] - send CMD
                     C^c, C^d - exit
                     help - show usage''')
+#na počátku je vypsáno jak interpretr používat                    
 showUsage()
 try:
     while True:
+        #čtení příkazů
         line = input('\nType a command: ')
-        words = line.split()
+        words = line.split() #rozdělení přečtené řádky na slova
         try:
+            #detekce klícových slov
+            
+            #výpis přidaných zařízení
             if (words[0] == 'devices'):
                 for key in devices:
                     print(key, end=', ')
                     print('addr: ' + str(devices[key].getAddr()))
+                    
+            #výpis nápovědy        
             elif (words[0] == 'help'):
                 showUsage()
+                
+            #přidání nového univerzáního modulu    
             elif (words[0] == 'add'):
+                #kontrola adresy
                 if (not words[2].isdecimal()):
                     print('Address is not a number: ' + words[2])
                     continue
+                #zařízení již bylo přidáno    
                 if (words[1] in devices):
                     print('Device already added: ' + words[1])
                     continue
+                    
+                #přidání modulu    
                 dev = pyhouse.Device(int(words[2]))
-
                 devices[words[1]] = dev
                 print(words[1] + ' added')
-                
+            
+						#odebrání modulu    
             elif (words[0] == 'del'):
+                #modulu neexistuje
                 if(words[1] not in devices):
                     print('Device does not exists: ' + words[1])
                     continue
+                
+								#odebrání    
                 del devices[words[1]]
                 print(words[1] + ' removed')
-                
+            
+						#provedení reset příkazu    
             elif(words[0] == 'reset'):
+                #modul neexistuje
                 if (words[1] not in devices):
                     print('Device does not exists: ' + words[1])
                     continue
                     
                 dev = devices[words[1]]    
                 dev.reset()
-										    
+						
+						#odeslání příkazu				    
             elif (words[0] == 'send'):
+                #modul neexistuje
                 if (words[1] not in devices):
                     print('Device not found: ' + words[1])
                     continue
+                    
+                #získání modulu    
                 dev = devices[words[1]]
+                
+                #přečtena 4 slova - je to send modul dev func, pokud ne metoda sendCmd nás vyvede z omylu a vyhodí výjímku
                 if(len(words) == 4):
-                    dev.sendCmd(words[2], words[3])
-                elif (len(words) == 5):
-                    if (not words[4].isdecimal()):
-                        print('Parameter is not number: ' + words[4])
-                        continue
                     try:
-                        dev.sendCmd(words[2], words[3], int(words[4]))
-                    except OverflowError:
-                        print ('Parameter out of range')
-                        continue                
+                        if(not dev.sendCmd(words[2], words[3])):
+                            print('CMD sending unsuccessfull')
+                    except Exception as e:
+                        print(e.args[0])
+                        continue
+                #přečteno 5 slov, je to send modul dev func param, pokud ne sendCmd vyhodí vyjímku  
+                elif (len(words) == 5):
+                    try:
+                        if(not dev.sendCmd(words[2], words[3], int(words[4]))):
+                            print('CMD sending unsuccessfull')
+                    except Exception as e:
+                        print(e.args[0]) 
+                        continue
+                #nesprávný počet slov         
                 else:
                     print('Invalid count of parameters')
+            
+						#nerozpoznáno klíčové slovo        
             else:
                 print('Unknow command: ' + words[0])
+        #pokus o přístup k chybějícím slovům       
         except IndexError:
             print('Incomplete command')
 
+#ukončení pomocí c^c nebo c^d a odebrání modulu pyhouse ze systémové cesty
 except KeyboardInterrupt:
+    sys.path.remove(os.path.dirname(os.path.abspath(__file__)))
     exit(0)
 except EOFError:
+    sys.path.remove(os.path.dirname(os.path.abspath(__file__)))
     exit(0)
--- src/Raspberry/pyhouse/pyhouse/sender.py
+++ src/Raspberry/pyhouse/pyhouse/sender.py
@@ -5,6 +5,7 @@ import serial
 import RPi.GPIO as GPIO
 import crcmod
 
+#slovník popisující podporovaná zařízení, jejich funkce, přijímání parametrů a kódování pro odesílání
 cmds = {'light1' : {'cmd'        : 1,
    
                     'on'         : {'par'       : False,
@@ -180,44 +181,49 @@ cmds = {'light1' : {'cmd'        : 1,
 	
        
        }
-
+#nastavení enable pinu pro ovládání směru komunikace po RS485
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 GPIO.output(12, GPIO.LOW)
 
-
+#třída pro komunikaci na RS485
 class RS485:
-    def __init__(self, OEPin, baudrate, timeout):
-        self.__ser = serial.Serial('/dev/ttyS0')
-        self.__ser.baudrate = baudrate
+    #konstruktor přijímá int baudrate a int timeout pro čtení dat v s
+    def __init__(self, baudrate, timeout):
+        self.__ser = serial.Serial('/dev/ttyS0')   #otevření sériové  linky
+        self.__ser.baudrate = baudrate             #nastavení parametrů
         self.__ser.timeout = timeout
-        self.__OE = OEPin
+        self.__OE = 12
         
-    
+    # metoda pro odeslání dat, data jsou typu byte
     def sendData(self, data):
         GPIO.output(self.__OE, GPIO.HIGH)
         self.__ser.write(data)
         self.__ser.flush()
         GPIO.output(self.__OE, GPIO.LOW)
-        
+    
+		#meoda pro čtení příchozích dat, parametr int cnt - počet dat k přijetí v B    
     def getData(self, cnt):
         data = self.__ser.read(cnt)
         return data
-        
+    
+		#metoda pro vyčištění příchozího bufferu    
     def clearInput(self):
         self.__ser.flushInput()
-        
+
+#třída pro práci s crc        
 class CRC:
+    #inicializace CRC
     def __init__(self):
         self.__crc = crcmod.mkCrcFun(0x11021, 0xffff, False, 0x0000)
-    
+    #vypočítá CRC pro vstupní data typu byte, vrátí vypočítanou hodnotu typu byte
     def getCrc(self, data):
         crc = self.__crc(data)
         crc = crc.to_bytes(2, 'big')
         return crc
 
-
+		#kontroluje CRC přijatých dat, parametrem jsou přijatá data typu byte obsahující CRC, vrací true/false
     def checkCrc(self, data):
         crc = self.getCrc(data[0:6])
         tmp = data[6:]
@@ -225,8 +231,9 @@ class CRC:
             return True
         return False
 
-
+#třída pro reprezentaci připojeného univerzálního modulu
 class Device:
+    #konstruktor přijímá int addr - adresu univerzálního modulu
     def __init__(self, addr):
         self.__cnt = 0
         self.__addr = addr
@@ -234,82 +241,103 @@ class Device:
         self.__crc = CRC()
         self.reset()
     
-        
-    def sendCmd(self,dev, func=None, param=None):
+     #metoda pro odeslání CMD, parametry jsou: str dev - nazev ovladaneho zarizeni, str func - název funkce k provedeni, int param - parametr funkce   
+    def sendCmd(self, dev, func=None, param=None):
         if (dev != 'reset'):
-            if (not self.__checkCmd(dev, func, param)):                         
-                print('Device, function or parameter was not recognized.')
-                return True
-    
+            self.__checkCmd(dev, func, param)  #kontrola vstupních parametrů                       
+                
+            #vytvoření dat k odeslání
+            #připojí se adresa cílové jednotky, číslo paketu, zařízení a funkce
             data = self.__addr.to_bytes(1, 'big') + self.__cnt.to_bytes(1, 'big') + cmds[dev]['cmd'].to_bytes(1, 'big') + cmds[dev][func]['cmd'].to_bytes(1, 'big')
-            if (self.__hasParam(dev, func)):                                                    
-                if (self.__hasTimeParam(dev, func)):                                        
+            #pokud funkce přijímá parametry, připojí se
+						if (self.__hasParam(dev, func)):                                                    
+                if (self.__hasTimeParam(dev, func)):  #připojení 2B časového parametru                                      
                     data = data + param.to_bytes(2, 'big')                                   
-                else:
+                else:   #připojení 1B parametru
                     data = data + param.to_bytes(1, 'big') + bytes.fromhex('00')        
-            else:
+            else: #funkce nepřijímá parametry
                 data = data + bytes.fromhex('00 00')
-        else:
+        else: #vytvoření reset příkazu
             data = self.__addr.to_bytes(1, 'big') + self.__cnt.to_bytes(1, 'big') + bytes.fromhex('00 00 00 00')
+         
+				#připojení CRC    
         data = data + self.__crc.getCrc(data)
     
+        #pokud po sběrnici přišlo něco nečekaného, například při debugování univerzálních modulů přes sériovou linku, je to ignorováno
         self.__ser.clearInput()
-        print("Sending cmd: ", end='')
-        print(data)
-        self.__ser.sendData(data)
+        #print("Sending cmd: ", end='')
+        #print(data)
+        self.__ser.sendData(data)   #odeslání dat
     
-        attempts = 10
+        #opakování odeslání zprávy, pokud nepřijde platné potvrzení nebo vyprší počet pokusů
+        attempts = 9
         while(True):
-                    if(attempts == 0):
-                        print('Error: cannot send CMD: ', end='')
-                        print(dev)
+                    if(attempts == 0): #vypršel počet pokusů, zprávu se nepodařilo odeslat
+                        #print('Error: cannot send CMD: ', end='')
+                        #print(dev)
                         return False
     
-                    if (not self.__checkAck()):   
-                        time.sleep(2)
-                        print('Sending CMD again: ' + str(data))
-                        self.__ser.sendData(data)
+                    if (not self.__checkAck()): #ack nepřišlo nebo přišla poškozená data  
+                        time.sleep(1) #čekání s novým odesláním 1s, pokud byla data porušená rušením, možná mezitím odezní
+                        #print('Sending CMD again: ' + str(data))
+                        self.__ser.sendData(data) #posílání zprávy znovu
                         attempts = attempts - 1
                     else:
-                        self.__cnt = (self.__cnt + 1) % 256
+                        self.__cnt = (self.__cnt + 1) % 256   #zpráva odeslána úspěšně, inkrementace čísla zprávy
                         return True
             
-   
+    #příkaz k resetování číslování posílaných paketů , vrátí true, pokud se povedlo spojit s univerzálním modulem a resetoat čítače, jinak vrátí false
     def reset(self):
-        self.sendCmd('reset')
-        self.__cnt = 0
-        
+        ret = self.sendCmd('reset')
+        if (ret):
+            self.__cnt = 0
+        return ret
+    
+		#vrací int adresu univerzáního modulu    
     def getAddr(self):
         return self.__addr
     
-    def __checkAck(self):        
+    #kontrola potvrzovacího paketu, vrací true, pokud je v pořádku, jinak false
+    def __checkAck(self): 
+		    #vytvoření očekávané zprávy k porovnání       
         expectedAck = bytes.fromhex('00') + self.__cnt.to_bytes(1, 'big') + bytes.fromhex('00') + self.__addr.to_bytes(1, 'big') + bytes.fromhex('00 00')    
         expectedAck = expectedAck + self.__crc.getCrc(expectedAck)
         
-        print('Waiting for ACK')
-        ack = self.__ser.getData(8)
-        print('Got: ' + str(ack))
+        #print('Waiting for ACK')
+        ack = self.__ser.getData(8) #čekání na příjem potvrzovacího paketu
+        #print('Got: ' + str(ack))
         
-        if (ack == expectedAck):
-            print('ACK OK')
+        if (ack == expectedAck):  #kontrola
+            #print('ACK OK')
             return True
         else:
-            print('Incorrect ACK, expected: ' + str(expectedAck))
+            #print('Incorrect ACK, expected: ' + str(expectedAck))
             return False
-    
+            
+    #kontrola, jestli jde o známé zařízení, funkci a jestli je v pořádku parameter, pokud ne, vyhodí Exception s popisem chyby v args[0]
     def __checkCmd(self, dev, func, param):
-        if (dev not in cmds):
-            return False
-        if (func not in cmds[dev]):
-            return False
-        if (param == None and cmds[dev][func]['par']):
-            return False
-        if (param != None and not cmds[dev][func]['par']):
-            return False
-        return True
+        if (dev not in cmds):  #kontrola existence zařízení
+            raise Exception('Unknown device')
+        if (func not in cmds[dev]):  #kontrola existence funkce
+            raise Exception('Unknown function')
+        if (param == None and self.__hasParam(dev, func)):  #parametr není a má být
+            raise Exception('Parameter expected')
+        if (param != None and not self.__hasParam(dev, func)):  #parametr je a nemá být
+            raise Exception('Unexpected parameter')
+        if (self.__hasParam(dev, func)):
+            if (not isinstance(param, int)):  #parametr není int
+                raise Exception('Invalid parameter, not int')
+            if (self.__hasTimeParam(dev, func)):
+                if (param < 0 or param > 65535): #časový parametr mimo rozsah
+                    raise Exception('Parameter out of range')
+            else:
+                if (param < 0 or param > 255):  #parametr mimo rozsah
+                    raise Exception('Parameter out of range')
     
+    #kontrola, jestli funkce nad daným zařízením přijímá parametr, vrací true/false
     def __hasParam(self, dev, func):
         return cmds[dev][func]['par']
     
+    #kontrola, jestli funkce nad daným zařízením přijímá časový parametr, vrací true/false
     def __hasTimeParam(self, dev, func):
         return cmds[dev][func]['time']
