093081e3a0915956d1cafc73ed95295f4066e655
Vojtěch Pail <vojta.pail@seznam.cz>
2018-05-01 21:11:26 +0200

    test

---
+++ src/Raspberry/test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import time
+
+#připojení modulu pyhouse do systémové cesty, aby mohl být importován
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+import pyhouse
+
+a = pyhouse.Device(1)
+b = pyhouse.Device(2)
+
+a.sendCmd('light1', 'on')
+time.sleep(5)
+a.sendCmd('light1', 'off')
+time.sleep(5)
+a.sendCmd('light1', 'dim', 1)
+a.sendCmd('light1', 'dim', 50)
+b.sendCmd('socket1', 'on')
+time.sleep(5)
+b.sendCmd('socket1', 'off')
+b.sendCmd('boiler', 'temp', 30)
+b.sendCmd('boiler', 'on')
+time.sleep(45)
+b.sendCmd('boiler', 'temp', 0)
+
+    
+
+	
+
+
+
+print('\nTerminating program...\n')
+GPIO.cleanup()
+sys.path.remove(os.path.dirname(os.path.abspath(__file__)))
+exit(0)
