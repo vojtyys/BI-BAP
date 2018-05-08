6429ae6af1e67cca7a04d8aace04d0d7bc78a1b0
Vojtěch Pail <vojta.pail@seznam.cz>
2018-05-08 17:04:08 +0200

    lights test created

--- src/Raspberry/install.sh
+++ src/Raspberry/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo "Installing python3, pip3"
+echo "Installing python3, pip3\n"
 sudo apt install python3 python3-pip
-echo "Installing setuptools"
+echo "Installing setuptools\n"
 sudo pip3 install setuptools
-echo "Installing API"
+echo "Installing API\n"
 sudo pip3 install ./pyhouse/
+echo "Install finished"
---
+++ src/Raspberry/test/lights.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import time
+import pyhouse
+
+#Adresa testované jednotky
+ADDR = 1
+
+def fail():
+    print("Test failed")
+    sys.exit(0)
+
+def commandFailed():
+    print('Cannot send command')
+    fail()
+    
+print('Starting test')
+a = pyhouse.Device(1)
+
+if(not a.reset()):
+    print("Connection between devices cannot be established")
+    fail()
+    
+print('Light1 full on')
+if(a.sendCmd('light1', 'on')):
+    commandFailed()
+
+print('Light2 full on')
+if(a.sendCmd('light2', 'on')):
+    commandFailed()
+    
+time.sleep(5)
+
+print('Light1 full off')
+if(a.sendCmd('light1', 'off')):
+    commandFailed()
+
+print('Light2 full off')
+if(a.sendCmd('light2', 'off')):
+    commandFailed()
+    
+time.sleep(5)
+
+print('Light1 dim 10 - light should stay off')
+if(a.sendCmd('light1', 'dim', 10)):
+    commandFailed()
+
+print('Light2 dim 10 - light should stay off')
+if(a.sendCmd('light2', 'dim', 10)):
+    commandFailed()
+    
+time.sleep(5)
+
+print('Light1 on - light should turn on and set dim level 10')
+if(a.sendCmd('light1', 'on')):
+    commandFailed()
+
+print('Light2 on - light should turn on and set dim level 10')
+if(a.sendCmd('light2', 'on')):
+    commandFailed()
+    
+time.sleep(5)
+
+print('Light1 full off')
+if(a.sendCmd('light1', 'off')):
+    commandFailed()
+
+print('Light2 full off')
+if(a.sendCmd('light2', 'off')):
+    commandFailed()
+    
+time.sleep(5)
+
+print('Light1 time test - light should turn on after 10 s and turn off after another 5 s')
+if(a.sendCmd('light1', 'timeon', 10)):
+    commandFailed()
+if(a.sendCmd('light1', 'timeoff', 15)):
+    commandFailed()
+
+print('Light2 time test - light should turn on after 10 s and turn off after another 5 s')
+if(a.sendCmd('light2', 'timeon', 10)):
+    commandFailed()
+if(a.sendCmd('light2', 'timeoff', 15)):
+    commandFailed()
+    
+time.sleep(9)
+print('Lights should turn on now')
+time.sleep(4)
+print('Lights should turn off now')
+
+time.sleep(5)
+print('Autoon test - cover BH1750 sensor by hand, lights should turn on in 30 s')
+if(a.sendCmd('light1', 'autoon')):
+    commandFailed()
+if(a.sendCmd('light2', 'autoon')):
+    commandFailed()
+
+time.sleep(29)
+print('Lights should be turned on')
+print("Lights test completed, try light buttons functionality.")
+exit(0)
